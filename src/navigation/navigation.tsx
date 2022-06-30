7a643dbac5f47c56ebed9bb57b18127417c5f836
Adam Pagels <pagels@hotmail.ca>
2022-06-30 16:18:08 +0200

    Set up navigation of Home and Movie screens

---
+++ src/navigation/navigation.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from '../screens/Home/Home';
+import Movie from '../screens/Movie/Movie';
+
+const Stack = createNativeStackNavigator();
+
+const Navigation = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+        initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Movie" component={Movie} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Navigation;
