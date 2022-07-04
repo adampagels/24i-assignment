dfe7fc7bde21e7ecbae4b39ee2db6ac964ebf55e
Adam Pagels <pagels@hotmail.ca>
2022-07-04 20:56:23 +0200

    Navigate to Movie Screen when CarouselItem is pressed

--- src/components/Carousel/CarouselItem.tsx
+++ src/components/Carousel/CarouselItem.tsx
@@ -1,25 +1,31 @@
 import React, { FC } from 'react';
-import { Text, View } from 'react-native';
-import { CarouselData, Movie } from "../../interfaces/interfaces"
+import { View } from 'react-native';
+import { CarouselData, Movie } from '../../interfaces/interfaces';
+import { MovieSceenNavigationProp } from '../../types/types';
 import { styles } from './Carousel.styles';
 import CarouselItemImage from './CarouselItemImage';
 import CarouselIteamTitle from './CarouselItemTitle';
-
+import { useNavigation } from '@react-navigation/native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 const CarouselItem: FC<CarouselData> = ({ items }) => {
+  const navigation = useNavigation<MovieSceenNavigationProp>();
+
   return (
     <>
       {items.map((movie: Movie) => {
         return (
-          <View style={styles.carouselItem}>
-            <CarouselItemImage movieImage={movie.posterUrl} />
-            <CarouselIteamTitle movieTitle={movie.title} />
-          </View>
-        )
+          <TouchableOpacity
+            onPress={() => navigation.navigate('Movie', { movie: movie })}>
+            <View style={styles.carouselItem}>
+              <CarouselItemImage movieImage={movie.posterUrl} />
+              <CarouselIteamTitle movieTitle={movie.title} />
+            </View>
+          </TouchableOpacity>
+        );
       })}
     </>
-  )
-
+  );
 };
 
 export default CarouselItem;
