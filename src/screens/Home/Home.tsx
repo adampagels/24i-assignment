5af0636e8f4ef74429d4ef9a639a3b6f9e4c9287
Adam Pagels <pagels@hotmail.ca>
2022-07-04 13:19:42 +0200

    Wrap carousels in SafeAreaView and ScrollView

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import { fetchedMovies } from '../../services/dataService';
 import Carousel from '../../components/Carousel/Carousel';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { ScrollView } from 'react-native-gesture-handler';
 
 const Home = () => {
   const fetchedMovieData = fetchedMovies();
   const movieInformation = fetchedMovieData?.carousels;
 
-  return movieInformation?.map(movie => {
-    return <Carousel title={movie.title} items={movie.items} />;
-  });
+  return (
+    <SafeAreaView>
+      <ScrollView alwaysBounceVertical={false}>
+        {movieInformation?.map(movie => {
+          return <Carousel title={movie.title} items={movie.items} />;
+        })}
+      </ScrollView>
+    </SafeAreaView>
+  );
 };
 
 export default Home;
