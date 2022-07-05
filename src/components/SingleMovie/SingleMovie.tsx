1cc2ce318655c026be46cc2e8dd0de00cbd52a6b
Adam Pagels <pagels@hotmail.ca>
2022-07-05 11:10:08 +0200

    Add container style and wrapinfo in ScrollView

--- src/components/SingleMovie/SingleMovie.tsx
+++ src/components/SingleMovie/SingleMovie.tsx
@@ -3,19 +3,21 @@ import { View } from 'react-native';
 import { Movie } from '../../interfaces/interfaces';
 import SingleMovieImage from './SingleMovieImage';
 import SingleMovieTitle from './SingleMovieTitle';
-import SingleMovieInfo from "./SingleMovieInfo"
+import SingleMovieInfo from './SingleMovieInfo';
 import { styles } from './SingleMovie.styles';
+import { ScrollView } from 'react-native-gesture-handler';
 
 const SingleMovie: FC<{ movie: Movie }> = ({ movie }) => {
-
   return (
     <>
-      <View>
+      <View style={styles.SingleMovieContainer}>
         <View style={styles.singleMovieTitleImageContainer}>
           <SingleMovieImage movieImage={movie.posterUrl} />
           <SingleMovieTitle movieTitle={movie.title} />
         </View>
-        <SingleMovieInfo movie={movie} />
+        <ScrollView alwaysBounceVertical={false}>
+          <SingleMovieInfo movie={movie} />
+        </ScrollView>
       </View>
     </>
   );
